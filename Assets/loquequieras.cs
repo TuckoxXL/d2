3e575d485f768806710db0c7943fc5ce49c8205b
TuckoxXL <sebastianbelluccini@gmail.com>
2022-09-05 14:39:43 -0300

    porfin
    agregue una capsula y deje un texto

--- Assets/loquequieras.cs
+++ Assets/loquequieras.cs
@@ -13,6 +13,6 @@ public class loquequieras : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        //cuesta mucho 
+        //cuesta mucho: cuesta muchisimo
     }
 }
